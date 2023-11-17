a8f37c275e8929d300ba709f464457f4aa101e05
S-Agalya <114995242+S-Agalya@users.noreply.github.com>
2023-11-17 20:35:14 +0530

    Update advent2.js

--- advent2.js
+++ advent2.js
@@ -1,31 +1,26 @@
-function input(){
+function converting_input_as_array(){
     const fs=require("node:fs")
     const content=fs.readFileSync("./input.txt","UTF-8")
     const a=content.split(/\r?\n/)
    return a
   //  console.log(a)
-  }input()
-  function opp_array(){
-    const n=input()
-    const opp=n.map(word=>word.charAt(0))
+  }
+  function separating_opponent1_from_col1(){
+   const opp=inputarray.map(word=>word.charAt(0))
             //  console.log(opp)  
               return opp 
-   } opp_array()
-   
-   function them_array(){
-     const n=input()
-     const your=n.map(word=>word.charAt(2))
+   } 
+   function separating_them_from_col1(){
+     const your=inputarray.map(word=>word.charAt(2))
               // console.log(your)  
               return your
-   }them_array()
-  
-  
-   function opponent_values() {
-    const opp_opts = opp_array();
-    for ( i = 0; i < opp_opts.length; i++) {
-        if (opp_opts[i] === 'A') {
-            opp_opts[i] = 1;
-           var opp1_values=opp_opts 
+   }
+     function allocating_points_to_opponent() {
+    
+    for ( i = 0; i < opponent.length; i++) {
+        if (opponent[i] === 'A') {
+            opponent[i] = 1;
+           var opp1_values=opponent 
         } 
         else if (opp1_values[i] === 'B') {
             opp1_values[i] = 2;
@@ -34,14 +29,14 @@ function input(){
             opp1_values[i] = 3;
         }
     }return opp1_values
-   }opponent_values()
+   }
   
-   function me_values(){
-    const your_opts=them_array()
-    for (let i = 0; i < your_opts.length; i++) {
-        if (your_opts[i] === 'Y') {
-            your_opts[i] = 2
-            var opp2_values=your_opts  
+   function allocating_points_to_me(){
+    
+    for (let i = 0; i < them.length; i++) {
+        if (them[i] === 'Y') {
+            them[i] = 2
+            var opp2_values=them  
         } 
         else if (opp2_values[i] === 'X') {
         opp2_values[i] = 1  
@@ -50,40 +45,45 @@ function input(){
             opp2_values[i]=3
         }
     }return opp2_values
-   }me_values()
+   }
   
-  function game(){
-      var ply1=opponent_values()
-      var ply2=me_values()
-      var win=6
-      var loss=0
-      var draw=3
+  function play(){
+    
+     var win=6
+     var loss=0
+     var draw=3
      var round_res1=0
      var round_res2=0
      var draw_pt=0
-      for(let i=0;i<ply1.length;i++){
-         for (j=0;j<ply2.length;j++){         //1=rock
-              if(ply1[i]!==ply2[i]){         //2=paper
-                                             //3=scissor
-                  if(ply1[i]==1 && ply2[i]==2 || ply1[i]==3 && ply2[i]==1 || ply1[i]==2 && ply2[i]==3){   
-                     round_res1+=ply2[i]+win 
+      for(let i=0;i<player1.length;i++){
+         for (j=0;j<player2.length;j++){         //1=rock
+              if(player1[i]!==player2[i]){         //2=paper                                            //3=scissor
+                  if(player1[i]==1 && player2[i]==2 || player1[i]==3 && player2[i]==1 || player1[i]==2 && player2[i]==3){   
+                     round_res1+=player2[i]+win 
                   }
-                  else if(ply1[i]==2 && ply2[i]==1 || ply1[i]==1 && ply2[i]==3 || ply1[i]==3 && ply2[i]==2) {
-                       round_res2+=ply2[i]+loss     
+                  else if(player1[i]==2 && player2[i]==1 || player1[i]==1 && player2[i]==3 || player1[i]==3 && player2[i]==2) {
+                       round_res2+=player2[i]+loss     
                   }       
                  }
               else {
-                  if(ply1[i]==ply2[i]){
-                       draw_pt+=ply2[i]+draw
+                  if(player1[i]==player2[i]){
+                       draw_pt+=player2[i]+draw                  
                  }
               }
             }
       }
-      var total=round_res1+round_res2+draw_pt
       console.log(round_res1)
       console.log(round_res2)
       console.log(draw_pt)
-      console.log(total)
-  }game()
+      var total=round_res1+round_res2+draw_pt
+   return total
+  }
+  var inputarray=converting_input_as_array()
+  var opponent=separating_opponent1_from_col1(inputarray)
+  var them=separating_them_from_col1(inputarray)
+  var player1=allocating_points_to_opponent(opponent)
+  var player2=allocating_points_to_me(them)
+  var game=play(player1,player2)
+  console.log(game)
   
   
